f00a4e5319cfd479ef9f71d86d5da5e07c4611f0
Andrey Abdulvaliev <plip1@yandex.ru>
2020-07-31 20:14:59 +0400

    изменение расположения ассетов. Диалог загрузки для элемента файла

--- src/app/file/file.component.ts
+++ src/app/file/file.component.ts
@@ -10,14 +10,21 @@ import * as uuid from 'uuid';
 })
 export class FileComponent implements OnInit {
   @Input() file: File;
+  @Input() selectedFiles: Array<File>;
   id: uuid;
   createDate: Date;
-  thumbnailPath: string | ArrayBuffer;
-  path: string | ArrayBuffer;
+  thumbnail: string;
+  path: string;
+  isShowProgress: boolean;
+  isShowMenu: boolean;
+  isShowOpenBtn: boolean;
+  isShowDeleteBtn: boolean;
+  isShowDownloadBBtn: boolean;
 
   constructor() {
     this.id = uuid.v4();
     this.createDate = new Date();
+    this.showProgress(true);
   }
 
   ngOnInit(): void {
@@ -35,23 +42,34 @@ export class FileComponent implements OnInit {
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = (event: ProgressEvent) => {
-          this.thumbnailPath = (event.target as FileReader).result;
+          this.thumbnail = (event.target as FileReader).result.toString();
+          this.path = (event.target as FileReader).result.toString();
+          console.log(this.path);
+          this.showProgress(false);
         };
         break;
       default:
-        this.thumbnailPath = 'test';
+        this.thumbnail = 'test';
     }
   }
 
   open(): void {
     console.log('OPEN()');
+    window.open(this.thumbnail, '_blank');
   }
 
-  remove(): void  {
-    console.log('remove()');
+  delete(): void {
+    console.log('delete()');
+    const index: number = this.selectedFiles.indexOf(this.file);
+    this.selectedFiles.splice(index, 1);
   }
 
   download(): void {
     console.log('download()');
+    this.showProgress(true);
+  }
+
+  showProgress(isShow: boolean): void {
+    this.isShowProgress = isShow;
   }
 }
